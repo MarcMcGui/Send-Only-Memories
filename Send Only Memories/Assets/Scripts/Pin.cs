c2a67c3f07b40eda9c20d22c2824ee810d2fa6da
jheim98 <jake.heimburger@du.edu>
2018-01-27 13:52:06 -0700

    Updated Pin functions in Pin script

--- Send Only Memories/Assets/Scripts/Pin.cs
+++ Send Only Memories/Assets/Scripts/Pin.cs
@@ -6,24 +6,29 @@ public class Pin : MonoBehaviour {
 
     public int location;
 
+    public Sprite normal;
+    public Sprite highlighted;
+    public SpriteRenderer spr;
+
     //Mouse over pin highlights
     //click opens menu
 
-    void onMouseDown() {
-        gameObject.SendMessage("open");
+    void onMouseOver() {
+        spr.sprite = highlighted;
+        if(Input.GetMouseButtonDown(0)) {
+            gameObject.SendMessage("open");
+            spr.sprite = normal;
+        }
     }
 
-    void onMouseOver() {
-        
+    void onMouseExit() {
+        spr.sprite = normal;
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        spr = GetComponent<SpriteRenderer>();
+        spr.sprite = normal;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
