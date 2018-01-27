6c0539373c3d60d344a2ca92a80857ea0002944d
LAPTOP-KI67A1ME\Marcus McGuire <Marcmcg1@icloud.com>
2018-01-27 12:11:58 -0700

    Letter menu added with script attached.

--- Send Only Memories/Assets/Scripts/GameManager.cs
+++ Send Only Memories/Assets/Scripts/GameManager.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    //public variables
+    public int weekTimer;
+
+    //private variables
+    private int apocalypseCountdown;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        apocalypseCountdown = 0;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //Increases the time to end if all weeks have been used. 
+        if ( weekTimer >= 14 )
+        {
+            apocalypseCountdown += 1;
+        }
+        
+        //transitions to next scene if end of 4 months
+		if ( apocalypseCountdown > 8)
+        {
+            End();
+        }
 	}
+
+    public void End()
+    {
+
+    }
 }
