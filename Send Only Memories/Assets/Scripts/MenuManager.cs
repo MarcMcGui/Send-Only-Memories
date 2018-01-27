108608aea516c2fa7ddb29c7484e1ffc476ccff2
LAPTOP-KI67A1ME\Marcus McGuire <Marcmcg1@icloud.com>
2018-01-26 23:49:16 -0700

    Adding THE GAME

---
+++ Send Only Memories/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
