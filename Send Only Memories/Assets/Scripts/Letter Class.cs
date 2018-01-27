513b28a3f9bbf0faade2b77a9f2f9c723b480520
DESKTOP-FA5Q37H\Chris <Christian.Anton@du.edu>
2018-01-27 13:46:35 -0700

    Updated the Letter class to have a container for the letter text file names -Christian

--- Send Only Memories/Assets/Scripts/Letter Class.cs
+++ Send Only Memories/Assets/Scripts/Letter Class.cs
@@ -7,28 +7,39 @@ public class LetterClass : MonoBehaviour
     //Amount of time it will take to send the letter (likely between 1 and 14 days)
     int resourcesNeeded;
 
-    //A key pointing to a sprite name in an array of sprites (both before and after being selected)
+    //Whether the player has selected the letter to be sent
+    bool selected = false;
+
+    //Whether the player has sent the letter (only changed at the end of the day)
+    bool sent = false;
+
+    //An array of sprites (both before and after being selected) that can be accesed by a key
     string[] spriteNames = new string[6] { "test0.png", "test1.png", "test2.png", "test3.png", "test4.png", "test5.png" };
     string[] spriteNamesSelected = new string[6] { "testS0.png", "testS1.png", "testS2.png", "testS3.png", "testS4.png", "testS5.png" };
 
-    //The name of the sprite file that will be used
-    string sprite;
-
-    //The index of the sprite of the letter in the array
+    //The key of the sprite of the letter in the array
     int spriteKey;
 
-    //Weather the player has selected the letter to be sent
-    bool selected = false;
+    //The name of the sprite file that will be used
+    string spriteFile;
 
-    //Weather the player has sent the letter (only changed at the end of the day)
-    bool sent = false;
+    //An array of the names of the txt files holding the letters that cane be acceded by a key
+    string[] letterTextNames = new string[7] { "letter0.txt", "letter1.txt", "letter3.txt", "letter4.txt", "letter5.txt", "letter6.txt" };
 
-	// Use this for initialization
-	void Start (int rn, int sk)
+    //The key of the text of the letter in the array
+    int letterKey;
+
+    //The name of the text file that will be used
+    string letterFile;
+
+	//Use this for initialization. Pass in the number of resources needed (rn), the key of the letter sprite (sk), and the key of the letter text (lk)
+	void Start (int rn, int sk, int lk)
     {
         resourcesNeeded = rn;
-        spriteKey = spriteKey;
-        sprite = spriteNames[sk];
+        spriteKey = sk;
+        letterKey = lk;
+        spriteFile = spriteNames[spriteKey];
+        letterFile = letterTextNames[letterKey];
 	}
 
     //When you select the letter, call this
