dad6d8c21f99390de7d9d5d30ad4d4df21b85b06
jheim98 <jake.heimburger@du.edu>
2018-01-27 12:32:38 -0700

    Created script for Pin object

---
+++ Send Only Memories/Assets/Scripts/Pin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public int location;
+
+    //Mouse over pin highlights
+    //click opens menu
+
+    void onMouseDown() {
+        gameObject.SendMessage("open");
+    }
+
+    void onMouseOver() {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
