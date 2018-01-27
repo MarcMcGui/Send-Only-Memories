7cee98a2ddb79cb8a61453a4ccb8afbcc1781f0f
LAPTOP-KI67A1ME\Marcus McGuire <Marcmcg1@icloud.com>
2018-01-27 15:04:53 -0700

    adding script to letter menu, etc.

--- Send Only Memories/Assets/Scripts/LetterMenu.cs
+++ Send Only Memories/Assets/Scripts/LetterMenu.cs
@@ -6,11 +6,26 @@ public class LetterMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Quit()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Submit()
+    {
+        
+    }
 }
