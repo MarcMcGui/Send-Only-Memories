31dddc36dcb06f60bf9808e5fc0b65c4ca557d99
DESKTOP-BKSSAEJ\RyanSFF-PC <Ryan.Dunagan@du.edu>
2018-01-28 14:04:03 -0700

    error fixes

--- Send Only Memories/Assets/Scripts/LetterClass.cs
+++ Send Only Memories/Assets/Scripts/LetterClass.cs
@@ -24,8 +24,6 @@ public class LetterClass : MonoBehaviour {
     void Start() {
         gm = FindObjectOfType<GameManager>();
 
-        texts[0].ToString();
-
        // index = -1;
 
         //changeSprite(gm.getIndex());
--- Send Only Memories/Assets/Scripts/TextSpawner.cs
+++ Send Only Memories/Assets/Scripts/TextSpawner.cs
@@ -6,8 +6,26 @@ public class TextSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        public class GameManager {
+        private static GameManager _instance;
+
+        public static GameManager Instance {
+            get {
+                if (_instance == null) {
+                    _instance = FindObjectOfType<GameManager>();
+                }
+
+                return _instance;
+            }
+        }
+
+        void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
 	
 	// Update is called once per frame
 	void Update () {
