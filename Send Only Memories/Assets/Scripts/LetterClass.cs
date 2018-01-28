2d8ba8b2fc8c03249f5f81f5238ea8d308b14609
DESKTOP-BKSSAEJ\RyanSFF-PC <Ryan.Dunagan@du.edu>
2018-01-28 00:11:38 -0700

    LetterClass update

--- Send Only Memories/Assets/Scripts/LetterClass.cs
+++ Send Only Memories/Assets/Scripts/LetterClass.cs
@@ -7,7 +7,7 @@ public class LetterClass : MonoBehaviour {
     public GameManager gm;
     public Sprite[] sprites;
     public string[] texts;
-    public string[] spriteNames = new string[21] {"1_boss", "C1_ace.txt", "C1_bd.txt", "C1_ace.txt", "C1_bd.txt", "C1_ace.txt", "C2_a.txt", "C2_b.txt", "C2_c.txt", "C2_d.txt", "C2_e.txt", "C2_f.txt", "C3_ace.txt", "C3_bd.txt", "C3_ace.txt", "C3_bd.txt", "C3_ace.txt", "1_ZackAubrey.txt", "1_RubenTerry.txt", "1_RubenBridget.txt", "1_PrinceAndrew.txt" };
+    public string[] spriteNames = new string[21] {"BOSS_LETTER_PLACEHOLDER", "C1_ace.txt", "C1_bd.txt", "C1_ace.txt", "C1_bd.txt", "C1_ace.txt", "C2_a.txt", "C2_b.txt", "C2_c.txt", "C2_d.txt", "C2_e.txt", "C2_f.txt", "C3_ace.txt", "C3_bd.txt", "C3_ace.txt", "C3_bd.txt", "C3_ace.txt", "1_ZackAubrey.txt", "1_RubenTerry.txt", "1_RubenBridget.txt", "1_PrinceAndrew.txt" };
 
     //Amount of time it will take to send the letter (likely between 1 and 14 days)
     int resourcesNeeded;
@@ -28,6 +28,8 @@ public class LetterClass : MonoBehaviour {
         for(int i = 0; i < sprites.Length; i++){
             texts[i] = System.IO.File.ReadAllText(spriteNames[i]);
         }
+
+        changeSprite(gm.getIndex());
     }
 
     // Update is called once per frame
